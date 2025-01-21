25d90504484eb8847306912cd3caecf295a88cfb
Colm Ukrainec <colmukrainec@gmail.com>
2025-01-21 15:11:45 -0600

    Initial commit for codebase: * Used `create-next-app` to initialize codebase * Added supabase-js to dependencies * Created util supabase-client.ts for basic setup * Added prettier * Configured eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals", // Next.js recommended rules
+    "next/typescript", // TypeScript support
+    "eslint:recommended", // General recommended rules
+    "plugin:prettier/recommended" // Prettier integration
+  ),
+  {
+    plugins: ["prettier"],
+    rules: {
+      // Prettier Rules
+      "prettier/prettier": [
+        "error",
+        {
+          semi: false, // No semicolons
+          singleQuote: true, // Single quotes for strings
+          trailingComma: "es5",
+          printWidth: 80,
+          tabWidth: 2,
+        },
+      ],
+
+      // Custom ESLint Rules
+      "semi": ["error", "never"], // Disallow semicolons
+      "@typescript-eslint/no-explicit-any": [
+        "error",
+        {
+          fixToUnknown: false, // Avoid suggesting `unknown`
+          ignoreRestArgs: false, // Disallow in rest arguments
+        },
+      ],
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-ignore": "allow-with-description", // Allow `@ts-ignore` only with description
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
