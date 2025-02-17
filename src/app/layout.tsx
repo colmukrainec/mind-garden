dac6f27b3a75f7fe8d2287a2713528f6bda0393b
jiminryuu <144750019+jiminryuu@users.noreply.github.com>
2025-02-16 21:00:31 -0600

    Add toastify pop up messages to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {children}
+        <ToastContainer position="top-right" autoClose={3000} />
         {children}
       </body>
     </html>
